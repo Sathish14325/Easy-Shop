900145f9bbd62f8d763aa0abee62fe137fba51a8
sathish14325 <sathishshankar631@gmail.com>
2024-11-26 18:23:07 +0530

    first stage development completed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Navbar, Nav, Container, Badge, NavDropdown } from 'react-bootstrap';
-import { FaShoppingCart, FaUser } from 'react-icons/fa';
-import { LinkContainer } from 'react-router-bootstrap';
-import { useSelector, useDispatch } from 'react-redux';
-import { useLogoutMutation } from '../slices/usersApiSlice';
-import { logout } from '../slices/authSlice';
-import { toast } from 'react-toastify';
-import SearchBox from './SearchBox';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Navbar, Nav, Container, Badge, NavDropdown } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
+import { toast } from "react-toastify";
+import SearchBox from "./SearchBox";
 
 const Header = () => {
-  const { cartItems } = useSelector(state => state.cart);
-  const { userInfo } = useSelector(state => state.auth);
+  const { cartItems } = useSelector((state) => state.cart);
+  const { userInfo } = useSelector((state) => state.auth);
   const [logoutApiCall] = useLogoutMutation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -21,8 +21,8 @@ const Header = () => {
       await logoutApiCall().unwrap();
       dispatch(logout());
 
-      navigate('/login');
-      toast.success('Logout successful');
+      navigate("/login");
+      toast.success("Logout successful");
     } catch (error) {
       toast.error(error?.data?.message || error.error);
     }
@@ -30,30 +30,30 @@ const Header = () => {
 
   return (
     <Navbar
-      bg='dark'
-      variant='dark'
-      expand='md'
+      bg="dark"
+      variant="dark"
+      expand="md"
       collapseOnSelect
-      className='fixed-top z-2 '
+      className="fixed-top z-2 "
     >
       <Container>
-        <LinkContainer to='/'>
-          <Navbar.Brand>MERN Shop</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand>Easy Shop</Navbar.Brand>
         </LinkContainer>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='ms-auto m-2'>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto m-2">
             <SearchBox />
-            <LinkContainer to='/cart'>
+            <LinkContainer to="/cart">
               <Nav.Link>
-                <FaShoppingCart style={{ marginRight: '5px' }} />
+                <FaShoppingCart style={{ marginRight: "5px" }} />
                 Cart
                 {cartItems.length > 0 && (
                   <Badge
                     pill
-                    bg='warning'
-                    style={{ marginLeft: '5px' }}
-                    className='text-dark'
+                    bg="warning"
+                    style={{ marginLeft: "5px" }}
+                    className="text-dark"
                   >
                     <strong>
                       {cartItems.reduce((acc, item) => acc + item.qty, 0)}
@@ -63,8 +63,8 @@ const Header = () => {
               </Nav.Link>
             </LinkContainer>
             {userInfo ? (
-              <NavDropdown title={`Hello👋, ${userInfo.name}`} id='username'>
-                <LinkContainer to='/profile'>
+              <NavDropdown title={`Hello👋, ${userInfo.name}`} id="username">
+                <LinkContainer to="/profile">
                   <NavDropdown.Item>Profile</NavDropdown.Item>
                 </LinkContainer>
                 <NavDropdown.Item onClick={logoutHandler}>
@@ -72,9 +72,9 @@ const Header = () => {
                 </NavDropdown.Item>
               </NavDropdown>
             ) : (
-              <LinkContainer to='/login'>
+              <LinkContainer to="/login">
                 <Nav.Link>
-                  <FaUser style={{ marginRight: '5px' }} />
+                  <FaUser style={{ marginRight: "5px" }} />
                   Sign In
                 </Nav.Link>
               </LinkContainer>
